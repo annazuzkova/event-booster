55bb7ea85499e38e7abf3060ad0f1241b5d634f0
maharvladyslav <128148368+maharvladyslav@users.noreply.github.com>
2025-02-12 18:05:27 +0200

    Add event fetching and rendering functionality with template support

--- src/js/api/get_events.js
+++ src/js/api/get_events.js
@@ -1,33 +1,40 @@
-export const getEvents = async params => {
-  const searchParams = new URLSearchParams({}); // тут будуть параметри запиту
+import { formatEvents } from '../helpers/format_events';
+export const getEvents = async (params = {}) => {
+  const {
+    currentPage = 0,
+    perPage = 20,
+    apiKey,
+    keyword,
+    countryCode,
+  } = params; //деструктуризація параметрів з присвоєнням дефолтних значень, якщо вони незаданні
 
-  const url =
-    'https://app.ticketmaster.com/discovery/v2/events.json?apikey=A8TfknWuvAEesY78luj7BLu0h4tXEN6d';
+  const queryParams = new URLSearchParams({
+    size: perPage, //кількість івентів на сторінці
+    page: currentPage, //поточна сторінка
+    apikey: apiKey,
+  }); // Обов'язкові араметри запиту
+
+  //Необов'язкові параметри
+  if (keyword) {
+    queryParams.set('keyword', keyword);
+  }
+  if (countryCode) {
+    queryParams.set('countryCode', countryCode);
+  }
+
+  const url = `https://app.ticketmaster.com/discovery/v2/events.json?${queryParams}`;
+
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Помилка при запиті івентів: ${response.status}`);
     }
+
     const data = await response.json();
-    if (!data._embedded || !data._embedded.events) {
-      return { page: data.page, events: [] };
-    }
-    const eventsList = data._embedded.events.map(event => {
-      return {
-        dates: event.dates,
-        id: event.id,
-        image: event.images
-          .filter(image => !image.fallback)
-          .sort((prev, next) => next.width - prev.width)[0],
-        name: event.name,
-      };
-    });
-    const events = {
-      page: data.page,
-      events: eventsList || [],
-    };
-    return events;
+
+    return formatEvents(data); // Формуємо об'єкт з інформацією про пагінацію та списком івентів за допомогою функції formatEvents та повертаємо його
   } catch (error) {
     console.error('Помилка при отримані івентів', error);
+    return {};
   }
 };

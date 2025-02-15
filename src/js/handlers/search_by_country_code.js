427ff70f19b35bceaffb268742160463b7499d57
maxvojcuk <maksimvojcuk09@gmail.com>
2025-02-15 19:42:30 +0200

    в розробці

---
+++ src/js/handlers/search_by_country_code.js
@@ -0,0 +1,20 @@
+import { getEvents } from '../api/get_events.js';
+
+import { renderEvents } from '../render/render_events.js';
+
+import { saveParams, loadParams } from '../helpers/storage.js';
+
+import eventsTemplate from 'bundel-text:../../templates/events.hbs';
+const API_KEY = 'A8TfknWuvAEesY78luj7BLu0h4tXEN6d';
+
+export const habdleCountryChange = async event => {
+  // окд обробки
+  const params = loadParams();
+  const countryCode = event.target.value;
+  const data = await getEvents({ ...params, apiKey: API_KEY, countryCode });
+};
+export const addCountryHandler = searchInput => {
+  if (searchInput) {
+    searchInput.addEventListener('change', habdleCountryChange);
+  }
+};

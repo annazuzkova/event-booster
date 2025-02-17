d1fbdcca53b22e41730cc2670e8a039c69abd48f
maxvojcuk <maksimvojcuk09@gmail.com>
2025-02-17 19:43:26 +0200

    Зробив функцію select

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,9 @@ import { getEvents } from './api/get_events';
 import { renderEvents } from './render/render_events';
 import eventsTemplate from 'bundle-text:../templates/events.hbs';
 import { renderCountrie } from './render/render_country_option';
+import { addCountryHandler } from './handlers/search_by_country_code';
 const API_KEY = 'A8TfknWuvAEesY78luj7BLu0h4tXEN6d'; //замінити ключ на ключ тімліда
+const select = document.querySelector('.form__select');
 
 const eventContainer = document.querySelector('[data-events]');
 renderCountrie();
@@ -16,3 +18,4 @@ const app = async () => {
 };
 
 app();
+addCountryHandler(select);
--- src/js/handlers/search_by_country_code.js
+++ src/js/handlers/search_by_country_code.js
@@ -3,19 +3,30 @@ import { getEvents } from '../api/get_events.js';
 import { renderEvents } from '../render/render_events.js';
 
 import { saveParams, loadParams } from '../helpers/storage.js';
+const eventContainer = document.querySelector('[data-events]');
 
 const select = document.querySelector('.form__select');
-// import eventsTemplate from 'bundel-text:../../templates/events.hbs';
+import eventsTemplate from 'bundle-text:../../templates/events.hbs';
 
 const API_KEY = 'A8TfknWuvAEesY78luj7BLu0h4tXEN6d';
 
 export const habdleCountryChange = async event => {
-  const params = loadParams();
   const countryCode = event.target.value;
-  const data = await getEvents({ ...params, apiKey: API_KEY, countryCode });
+  const params = loadParams();
+  try {
+    if (countryCode) {
+      const data = await getEvents({ ...params, apiKey: API_KEY, countryCode });
+      saveParams({ ...params, countryCode });
+      renderEvents(data.events, eventContainer, eventsTemplate);
+    }
+  } catch (error) {
+    console.error('Error finding events:', error);
+    container.innerHTML =
+      '<p class="error-message__wrong">Something went wrong.</p>';
+  }
 };
-export const addCountryHandler = searchInput => {
-  if (searchInput) {
-    searchInput.addEventListener('change', habdleCountryChange);
+export const addCountryHandler = searchSelect => {
+  if (searchSelect) {
+    searchSelect.addEventListener('change', habdleCountryChange);
   }
 };
